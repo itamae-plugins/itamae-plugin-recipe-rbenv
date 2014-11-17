c164d92f21d79da9b821837cb07d71890b7cc990
Takashi Kokubun <takashikkbn@gmail.com>
2014-11-17 23:17:35 +0900

    Bundle gem itamae-plugin-recipe-rbenv

---
+++ lib/itamae/plugin/recipe/rbenv.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/rbenv/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Rbenv
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/rbenv/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module Rbenv
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
