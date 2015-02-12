e6e52fd3bf29779335e691791c8fe344602f4ad8
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2015-02-12 15:08:01 +0900

    Refactor: CONSTANT to method
    for reference from own recipe

--- lib/itamae/plugin/recipe/rbenv/system.rb
+++ lib/itamae/plugin/recipe/rbenv/system.rb
@@ -2,18 +2,13 @@ package "libffi-devel"
 package "openssl-devel"
 package "readline-devel"
 
-RBENV_ROOT = "/usr/local/rbenv"
-RBENV_INIT = <<-EOS
-  export RBENV_ROOT=#{RBENV_ROOT}
-  export PATH="#{RBENV_ROOT}/bin:${PATH}"
-  eval "$(rbenv init --no-rehash -)"
-EOS
+require "itamae/plugin/recipe/rbenv"
 
-git RBENV_ROOT do
+git rbenv_root do
   repository "git://github.com/sstephenson/rbenv.git"
 end
 
-git "#{RBENV_ROOT}/plugins/ruby-build" do
+git "#{rbenv_root}/plugins/ruby-build" do
   repository "git://github.com/sstephenson/ruby-build.git"
   if node[:'ruby-build'] && node[:'ruby-build'][:revision]
     revision node[:'ruby-build'][:revision]
@@ -22,14 +17,14 @@ end
 
 node[:rbenv][:versions].each do |version|
   execute "rbenv install #{version}" do
-    command "#{RBENV_INIT} rbenv install #{version}"
-    not_if  "#{RBENV_INIT} rbenv versions | grep #{version}"
+    command "#{rbenv_init} rbenv install #{version}"
+    not_if  "#{rbenv_init} rbenv versions | grep #{version}"
   end
 end
 
 node[:rbenv][:global].tap do |version|
   execute "rbenv global #{version}" do
-    command "#{RBENV_INIT} rbenv global #{version}"
-    not_if  "#{RBENV_INIT} rbenv version | grep #{version}"
+    command "#{rbenv_init} rbenv global #{version}"
+    not_if  "#{rbenv_init} rbenv version | grep #{version}"
   end
 end
