dbea209a97a649ae49277bf3f6220e9ff1147228
Takashi Kokubun <takashikkbn@gmail.com>
2015-12-07 00:52:33 +0900

    Move rbenv.rb contents to system.rb

--- lib/itamae/plugin/recipe/rbenv.rb
+++ lib/itamae/plugin/recipe/rbenv.rb
@@ -1,17 +1,1 @@
-DEFAULT_RBENV_ROOT = "/usr/local/rbenv"
-
-def rbenv_root
-  if node[:rbenv] && node[:rbenv][:rbenv_root]
-    node[:rbenv][:rbenv_root]
-  else
-    DEFAULT_RBENV_ROOT
-  end
-end
-
-def rbenv_init
-  <<-EOS
-    export RBENV_ROOT=#{rbenv_root}
-    export PATH="#{rbenv_root}/bin:${PATH}"
-    eval "$(rbenv init --no-rehash -)"
-  EOS
-end
+require "itamae/plugin/recipe/rbenv/version"
--- lib/itamae/plugin/recipe/rbenv/system.rb
+++ lib/itamae/plugin/recipe/rbenv/system.rb
@@ -38,7 +38,23 @@ package "git"
 scheme = "git"
 scheme = node[:rbenv][:scheme] if node[:rbenv][:scheme]
 
-require "itamae/plugin/recipe/rbenv"
+DEFAULT_RBENV_ROOT = "/usr/local/rbenv"
+
+def rbenv_root
+  if node[:rbenv] && node[:rbenv][:rbenv_root]
+    node[:rbenv][:rbenv_root]
+  else
+    DEFAULT_RBENV_ROOT
+  end
+end
+
+def rbenv_init
+  <<-EOS
+    export RBENV_ROOT=#{rbenv_root}
+    export PATH="#{rbenv_root}/bin:${PATH}"
+    eval "$(rbenv init --no-rehash -)"
+  EOS
+end
 
 git rbenv_root do
   repository "#{scheme}://github.com/rbenv/rbenv.git"
